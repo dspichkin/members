5a19bd017d805a864d3d7e529ae6bff87cb1f4a1
Denis Y. Spichkin <ds@levellab.ru>
2018-05-23 08:14:05 +0300

    change settings

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,17 +1,7 @@
 export class AppSettings {
-    static host = document.location.hostname;
-    static protocol = document.location.protocol;
-    
-    public static base_url;
     public static URL_MEMBERS;
     
     static initialized() {
-
-        if (AppSettings.host.indexOf('localhost') > -1 || AppSettings.host.indexOf('127.0.0.1') > -1) {
-            AppSettings.base_url = AppSettings.protocol + '//' + AppSettings.host + ':8000';
-        } else {
-            AppSettings.base_url = AppSettings.protocol + '//' + AppSettings.host;
-        }
 
         AppSettings.URL_MEMBERS = '/assets/generated.json';
 
